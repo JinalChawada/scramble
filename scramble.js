f59ed1142be7f8c7472f83d403d1dcc203317cc9
Jinal Chawada <55105846+JinalChawada@users.noreply.github.com>
2020-06-24 23:08:19 -0400

    Add files via upload
    Updated

--- scramble.js
+++ scramble.js
@@ -68,7 +68,20 @@ function help () {
 /**********************************************
  * YOUR CODE GOES BELOW
  **********************************************/
-
+// Create  word array with 40 or more words
+/**
+ * Game Object 
+ 
+The game active status
+The random list of words
+The current word
+The current scrambled word
+The number of strikes
+The number of points
+The maximum number of allowed strikes
+The number of passes used
+The maximum number of passes
+ */
 // the game object
 const game = {
   maxStrikes: 3,
@@ -76,3 +89,51 @@ const game = {
 }
 
 console.log(help())
+/** Start Function
+ No parameters
+Reset all the game status properties
+Make a copy of the words array and store it in the game object
+Shuffle the words array copy. A shuffle function was provided in the starter files.
+Select a word from the shuffled array save to the game object.
+Display a scrambled version of the word to the player.
+Note : If a game currently active than a new game CANNOT be started.
+ */
+function start(){
+
+}
+/**'
+ Guess Function
+
+Check to see if the guessed word matches the current word unscrambled. Case should NOT matter.
+If the words do match the player will receive a point. The word should be removed from the game list (the same word should not appear twice) and the next word should be displayed.
+If the words do NOT match the player will receive a strike and the current word will be displayed again.
+
+Note: The player should NOT be able to make guesses if there is no active game
+Parameter:  The user guess
+ */
+
+ function guess(word){
+
+ }
+ /** 
+Pass Function
+  
+Check if player has any passes left
+Remove the from the game list (the same word should not appear twice) and the next word should be displayed.
+Parameter: No parameters 
+
+Note : The player should NOT be able to pass if there is no active game.
+*/
+
+  function pass(){
+
+  }
+
+  /**
+   * Additional Notes:
+   * 
+The game will end if there are no more words in the list OR the player has received the maximum number of strikes
+When the game ends the player total points should be displayed.
+After the game ends the player should be able to start a new game using the start() function
+   */
+
